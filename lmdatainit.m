fa7690f25ebc9efcf9a6f0697a5714da711c2424
Ji Yu <jyu@uchc.edu>
2019-03-29 09:03:09 -0400

    change lmdatainit.m interface to allow more flexible parameters

--- lmdatainit.m
+++ lmdatainit.m
@@ -1,32 +1,63 @@
-function dataobj = lmdatainit(locdata, pixelsize, edges, numsigmabins)
-% dataobj = lmdatainit(locdata, pixelsize, edges, numsigmabins)
+function dataobj = lmdatainit(varargin)
+% dataobj = lmdatainit(locdata, pixelsize, padding, numsigmabins)
+% dataobj = lmdatainit(locdata, edges, padding, numsigmabins)
 % Prepare data to be used for other fuctions
 %
 % Inputs -
 %   locdata - 3 x N matrix. X, Y, Sigma. In same unit (e.g. nm)
 %   pixlesize - the pixel size of the image to be rendered
-%   edges - Optional. Bin edges used to discretize localization data
+%   edges - Bin edges used to discretize localization data.
+%   padding - Optional. default is using the largest psf size
 %   numsigmabins - Optional. default 50.
 %
 % Outputs -
 %   img - deconv img
 
 narginchk(1,4);
+locdata = varargin{1};
+if (size(locdata,1) < 3)
+    error('input data should have 3 rows');
+end
 
-if (~exist('numsigmabins','var'))
+if (length(varargin) < 4)
     numsigmabins = 50;
+else
+    numsigmabins = varargin{4};
 end
 
 if(~isscalar(numsigmabins))
     error('numsigmabins must be a scalar');
 end
 
-if (~exist('pixelsize','var'))
-    pixelsize = 8;
+if (length(varargin) >= 3)
+    padding = varargin{3};
+    if (isscalar(padding))
+        padding = [padding padding];
+    end
+    if (length(padding) ~= 2)
+        error('Wrong padding size');
+    end
 end
-
-if(~isscalar(pixelsize))
-    error('pixelsize must be a scalar');
+ 
+if (length(varargin) < 2)
+    pixelsize = 8;
+else
+    if (isscalar(varargin{2}))
+        pixelsize = varargin{2};
+    else
+        edges = varargin{2};
+        if (~iscell(edges))
+            error('edges should be a 2-element cell array');
+        end
+        xedges = edges{1};
+        yedges = edges{2};
+        dxedges = diff(xedges);
+        dyedges = diff(yedges);
+        if (range(dxedges) ~= 0 || range(dyedges) ~= 0 || dxedges(1) ~= dyedges(1))
+            error('edges should be equal spacing');
+        end
+        pixelsize = dxedges(1);
+    end
 end
 
 [sigmabins, sigmaedges] = discretize(locdata(3,:),numsigmabins);
@@ -37,18 +68,19 @@ for i = 1:size(psfs,3)
     psfs(:,:,i) = fspecial('gaussian', psfsize, 0.5 /pixelsize * (sigmaedges(i) + sigmaedges(i+1)));
 end
 
-if (~exist('edges','var'))
+if (~exist('xedges','var'))
     xedges = floor(min(locdata(1,:))/pixelsize)*pixelsize:pixelsize:max(locdata(1,:))+pixelsize;
     yedges = floor(min(locdata(2,:))/pixelsize)*pixelsize:pixelsize:max(locdata(2,:))+pixelsize;
-else
-    xedges = edges{1} * pixelsize;
-    yedges = edges{2} * pixelsize;
 end
 xbins = discretize(locdata(1,:),xedges);
 ybins = discretize(locdata(2,:),yedges);
 
 img = histcounts2(locdata(2,:), locdata(1,:), yedges, xedges);
-img = double(padarray(img, [psfhsize, psfhsize], 'both'));
+
+if (~exist('padding','var'))
+    padding = [psfhsize, psfhsize];
+end
+img = double(padarray(img, padding, 'both'));
 
 data = zeros(3, size(locdata,2), 'uint32');
 data(1,:) = xbins + psfhsize - 1;
@@ -60,3 +92,5 @@ dataobj.data = uint32(data);
 dataobj.initimg = double(img);
 dataobj.psfs = double(psfs);
 dataobj.pixelsize = pixelsize;
+dataobj.origin = locdata;
+dataobj.edges = {xedges, yedges};
