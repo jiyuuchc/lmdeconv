0ec67270b6d8712812fdb2751687946205e54515
Ji Yu <jyu@uchc.edu>
2019-04-01 14:25:34 -0400

    lmsample.m now take an optional prior

--- lmsample.m
+++ lmsample.m
@@ -1,4 +1,4 @@
-function samples = lmsample(lmobj, iters, prev, skip)
+function samples = lmsample(lmobj, iters, prev, skip, prior)
 % samples = lmdsample(locdata, iters, prev, skip)
 % Sample the posterior distribution on theta based on SMLM data
 %
@@ -7,11 +7,16 @@ function samples = lmsample(lmobj, iters, prev, skip)
 %   iter - # of samples to be drawn
 %   prev - Optional. result from previos iteration. 
 %   skip - Optional. skipping samples, default 1
+%   prior - Dir prior. can be a scaler or a matrix.
 %
 % Outputs -
 %   samples - samples
 
-narginchk(2,4);
+narginchk(2,5);
+
+if (~exist('prior','var'))
+    prior = 1.0;
+end
 
 if (~exist('skip','var'))
     skip = 1;
@@ -19,7 +24,6 @@ else
     if (~isscalar(skip))
         error ('skip must be a scalar');
     end
-    
 end
 
 if(~isscalar(iters))
@@ -30,10 +34,14 @@ if(~exist('prev','var') || isempty(prev))
     prev = lmobj.initimg;
 end
 
+if (~isscalar(prior) && numel(prior) ~= numel(prev))
+    error('Prior size does not match image');
+end
+
 [h,w] = size(prev);
 samples = zeros(h,w,iters);
 for i = 1:iters * skip
-    prev = lmsamplermex(lmobj.data, prev, lmobj.psfs);
+    prev = lmsamplermex(lmobj.data, prev, lmobj.psfs, double(prior));
     if (mod(i,skip)==0)
         samples(:,:,i/skip) = prev;
     end
