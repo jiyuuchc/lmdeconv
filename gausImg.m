d5520635708fde09ab0d9c5d67f37967c43b5742
Ji Yu <jyu@uchc.edu>
2019-07-23 16:53:09 -0400

    add gaussianspot render function

---
+++ gausImg.m
@@ -0,0 +1,16 @@
+function img=gausImgRendr(lmobj)
+% render Gaussian spot image using lmobj as input
+
+img = zeros(size(lmobj.initimg));
+[h0,w0]=size(img);
+[h,w,~]=size(lmobj.psfs);
+wh = (w-1)/2;
+hh = (h-1)/2;
+for i = 1:length(lmobj.data)
+    x0 = lmobj.data(1,i)+1;
+    y0 = lmobj.data(2,i)+1;
+    p0 = lmobj.data(3,i)+1;
+    if (x0 > wh && x0+wh <= w0 && y0 > hh && y0+hh <= h0)
+        img(y0-hh:y0+hh,x0-wh:x0+wh) = img(y0-hh:y0+hh,x0-wh:x0+wh) + lmobj.psfs(:,:,p0);
+    end
+end
