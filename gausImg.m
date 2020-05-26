410c9e1eeb163a07611b8b5cbe7b9d586fc223e7
Ji Yu <jyu@uchc.edu>
2020-05-25 21:52:32 -0400

    code clean up

--- gausImg.m
+++ gausImg.m
@@ -6,16 +6,17 @@ function img=gausimg(lmobj)
 % output:
 %   img: Rendered image.
 
-img = zeros(size(lmobj.initimg));
+img = zeros(lmobj.imgsize);
 [h0,w0]=size(img);
-[h,w,~]=size(lmobj.psfs);
-wh = (w-1)/2;
-hh = (h-1)/2;
 for i = 1:length(lmobj.data)
     x0 = lmobj.data(1,i)+1;
     y0 = lmobj.data(2,i)+1;
     p0 = lmobj.data(3,i)+1;
+    psf = lmobj.psfs{p0};
+    [h,w]=size(psf);
+    wh = (w-1)/2;
+    hh = (h-1)/2;
     if (x0 > wh && x0+wh <= w0 && y0 > hh && y0+hh <= h0)
-        img(y0-hh:y0+hh,x0-wh:x0+wh) = img(y0-hh:y0+hh,x0-wh:x0+wh) + lmobj.psfs(:,:,p0);
+        img(y0-hh:y0+hh,x0-wh:x0+wh) = img(y0-hh:y0+hh,x0-wh:x0+wh) + psf;
     end
 end
