6bbf3b4a47d4dd8fbf9d96722f6b1ea69b64fe96
Ji Yu <jyu@uchc.edu>
2020-07-22 10:37:43 -0400

    clean up parser code for lmdeconv.m

--- lmdeconv.m
+++ lmdeconv.m
@@ -1,4 +1,4 @@
-function img = lmdeconv(lmobj, iters, prev, prior)
+function img = lmdeconv(lmobj, varargin)
 % img = lmdeconv(locdata, iters, prev, prior)
 % Perform statitically accurate rendering of SMLM data using ML estimator
 % 
@@ -10,15 +10,19 @@ function img = lmdeconv(lmobj, iters, prev, prior)
 % Outputs -
 %   img - deconv img
 
-narginchk(2,4);
+parser = inputParser;
+isPosInt = @(p) validateattributes(p, {'numeric'},{'scalar','positive','integer'});
+addRequired(parser, 'lmobj', @(p) isstruct(p));
+addOptional(parser, 'iters', 100, isPosInt);
+addParameter(parser, 'Init', [], @(p) validateattributes(p, {'numeric'},{'2d'}));
+addParameter(parser, 'Prior', .5, @(p) isnumeric(p) && (isscalar(p) || ismatrix(p)));
+addParameter(parser, 'Quite', false, @(p) islogical(p) && isscalar(p));
+parse(parser, lmobj, varargin{:});
 
-if(~isscalar(iters))
-    error('iters must be a scalar');
-end
-if(~exist('prior','var'))
-    prior=1;
-end
-if(~exist('prev','var') || isempty(prev))
+iters = parser.Results.iters;
+prev = parser.Results.Init;
+prior = parser.Results.Prior;
+if(isempty(prev))
     prev = ones(lmobj.imgsize);
 end
 
@@ -28,7 +32,7 @@ for i = 1:iters
     else
         prev = lmdeconv3dmex(lmobj.data, prev, lmobj.psfs, lmobj.zpsfs, double(prior));
     end
-    if (mod(i,10) == 0)
+    if (mod(i,50) == 0 && ~ parser.Results.Quite)
         disp(['finished ' int2str(i) ' iterations']);
     end
 end
